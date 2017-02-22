68078cc195a911d4041a7c09ff25a39e38515739
Mike MacCana <mike@certsimple.com>
2017-02-22 11:44:49 +0000

    Tabs not spaces

--- index.js
+++ index.js
@@ -7,53 +7,53 @@ require('es6-shim')
 var log = console.log.bind(console);
 
 function parseRawData(rawData) {
-    var result = {};
-    var lines = rawData.split(os.EOL);
+	var result = {};
+	var lines = rawData.split(os.EOL);
 
-    lines.forEach(function(line){
-        line = line.trim();
-        if ( line && line.includes(': ') ) {
-            var lineParts = line.split(':');
+	lines.forEach(function(line){
+		line = line.trim();
+		if ( line && line.includes(': ') ) {
+			var lineParts = line.split(':');
 
-            // greater than since lines often have more than one colon, eg values with URLS
-            if ( lineParts.length >= 2 ) {
-                var keyName = changeCase.camelCase(lineParts[0]);
-                if ( !(keyName in result) )
-                  result[keyName] = [];
-                result[keyName].push(lineParts.splice(1).join(':').trim());
-            }
-        }
-    });
+			// greater than since lines often have more than one colon, eg values with URLS
+			if ( lineParts.length >= 2 ) {
+				var keyName = changeCase.camelCase(lineParts[0]);
+				if ( !(keyName in result) )
+				  result[keyName] = [];
+				result[keyName].push(lineParts.splice(1).join(':').trim());
+			}
+		}
+	});
 
-    return result;
+	return result;
 }
 
 module.exports = function(domain, options, cb){
 
-    if ( typeof cb === 'undefined' && typeof options === 'function' ) {
-        cb = options;
-        options = {};
-    }
+	if ( typeof cb === 'undefined' && typeof options === 'function' ) {
+		cb = options;
+		options = {};
+	}
 
-    whois.lookup(domain, options, function(err, rawData) {
+	whois.lookup(domain, options, function(err, rawData) {
 
-        if ( err ) {
-            return cb(err, null);
-        }
+		if ( err ) {
+			return cb(err, null);
+		}
 
-        var result = {};
+		var result = {};
 
-        if ( typeof rawData == 'object' ) {
-            result = rawData.map(function(data) {
-                data.data = parseRawData(data.data);
-                return data;
-            });
-        } else {
-            result = parseRawData(rawData);
-        }
+		if ( typeof rawData == 'object' ) {
+			result = rawData.map(function(data) {
+				data.data = parseRawData(data.data);
+				return data;
+			});
+		} else {
+			result = parseRawData(rawData);
+		}
 
-        cb(null, result);
-    });
+		cb(null, result);
+	});
 }
 
 
